82ae6f5583167c4e513edc39cb61b861ee371d0f
GrinBik <gregorybik@gmail.com>
2023-08-17 15:32:14 +0400

    Fix TOKEN

--- tel_bot.py
+++ tel_bot.py
@@ -3,7 +3,7 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv('TELTOKEN')
 bot = telebot.TeleBot(TOKEN)
 
 keyboard = InlineKeyboardMarkup()
